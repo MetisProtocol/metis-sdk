274fb1d462da4561abfaa550f01b13ca3a0c8bcc
nora-coder-dot <nora6677@gmail.com>
2025-03-20 12:56:41 +0800

    feat: add async compile pool and cache
    Signed-off-by: nora-coder-dot <nora6677@gmail.com>

--- crates/vm/src/lib.rs
+++ crates/vm/src/lib.rs
@@ -1,6 +1,15 @@
 #[cfg(feature = "compiler")]
 pub mod compiler;
 #[cfg(feature = "compiler")]
-pub use compiler::{CompilerContext, ExternalContext, register_compile_handler};
+pub use compiler::{CompilerContext, ExtCompileWorker, register_compile_handler};
+#[cfg(feature = "compiler")]
+pub mod pool;
+#[cfg(feature = "compiler")]
+mod runtime;
+
+pub use error::Error;
 
 pub mod analysis;
+pub mod env;
+pub mod error;
+pub mod hotcode;
---
+++ crates/vm/src/runtime.rs
@@ -0,0 +1,16 @@
+use std::sync::Once;
+use tokio::runtime::Runtime;
+
+static mut RUNTIME: Option<Runtime> = None;
+static RUNTIME_INIT: Once = Once::new();
+
+/// Makes sure only a single runtime thread is alive throughout the program lifetime.
+#[allow(static_mut_refs)]
+pub(crate) fn get_runtime() -> &'static Runtime {
+    unsafe {
+        RUNTIME_INIT.call_once(|| {
+            RUNTIME = Some(Runtime::new().unwrap());
+        });
+        RUNTIME.as_ref().unwrap()
+    }
+}
