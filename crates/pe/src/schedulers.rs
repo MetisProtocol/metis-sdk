ef577bb6c0c6344ca75fa9a48738f8f9251d6f0a
kilb <github@bll.io>
2025-04-09 23:08:22 +0800

    use AtomicUsize

--- crates/pe/src/schedulers.rs
+++ crates/pe/src/schedulers.rs
@@ -6,7 +6,7 @@ use std::sync::{
 
 use smallvec::SmallVec;
 
-use crate::{FinishExecFlags, IncarnationStatus, Task, TxIdx, TxStatus, TxVersion};
+use crate::{AtomicWrapper, FinishExecFlags, IncarnationStatus, Task, TxIdx, TxStatus, TxVersion};
 
 #[derive(Debug)]
 pub struct TransactionsGraph {
@@ -193,7 +193,7 @@ pub(crate) struct ExeScheduler<T: TaskProvider> {
     // TODO: Consider packing [TxStatus]s into atomics instead of
     // [Mutex] given how small they are.
     // TODO2: use AtomicUsize
-    transactions_status: Vec<Mutex<TxStatus>>,
+    transactions_status: Vec<AtomicWrapper<TxStatus>>,
     /// The list of dependent transactions to resume when the
     /// key transaction is re-executed.
     // TODO2: USE Graph or other data structure to store the dependencies
@@ -210,7 +210,7 @@ impl<T: TaskProvider> ExeScheduler<T> {
             execution_queue: SegQueue::new(),
             transactions_status: (0..block_size)
                 .map(|_| {
-                    Mutex::new(TxStatus {
+                    AtomicWrapper::new(TxStatus {
                         incarnation: 0,
                         status: IncarnationStatus::ReadyToExecute,
                     })
@@ -222,13 +222,28 @@ impl<T: TaskProvider> ExeScheduler<T> {
     }
 
     fn try_execute(&self, tx_idx: TxIdx) -> Option<TxVersion> {
-        let mut tx = index_mutex!(self.transactions_status, tx_idx);
-        if tx.status == IncarnationStatus::ReadyToExecute {
-            tx.status = IncarnationStatus::Executing;
-            return Some(TxVersion {
-                tx_idx,
-                tx_incarnation: tx.incarnation,
-            });
+        let tx = self.transactions_status.get(tx_idx).unwrap();
+        let old_status = tx.load(Ordering::Relaxed);
+
+        if old_status.status == IncarnationStatus::ReadyToExecute {
+            let incarnation = old_status.incarnation;
+            if tx
+                .compare_exchange(
+                    old_status,
+                    TxStatus {
+                        incarnation,
+                        status: IncarnationStatus::Executing,
+                    },
+                    Ordering::Relaxed,
+                    Ordering::Relaxed,
+                )
+                .is_ok()
+            {
+                return Some(TxVersion {
+                    tx_idx,
+                    tx_incarnation: incarnation,
+                });
+            }
         }
         None
     }
@@ -259,17 +274,19 @@ impl<T: TaskProvider> ExeScheduler<T> {
     pub(crate) fn add_dependency(&self, tx_idx: TxIdx, blocking_tx_idx: TxIdx) -> bool {
         // This is an important lock to prevent a race condition where the blocking
         // transaction completes re-execution before this dependency can be added.
-        let blocking_tx = index_mutex!(self.transactions_status, blocking_tx_idx);
+        let tx = self.transactions_status.get(blocking_tx_idx).unwrap();
+        let old_status = tx.load(Ordering::Relaxed);
         if matches!(
-            blocking_tx.status,
+            old_status.status,
             IncarnationStatus::Executed | IncarnationStatus::Validated
         ) {
             return false;
         }
 
-        let mut tx = index_mutex!(self.transactions_status, tx_idx);
-        debug_assert_eq!(tx.status, IncarnationStatus::Executing);
-        tx.status = IncarnationStatus::Blocking;
+        let tx = self.transactions_status.get(tx_idx).unwrap();
+        let mut tx_status = tx.load(Ordering::Relaxed);
+        tx_status.status = IncarnationStatus::Blocking;
+        tx.store(tx_status, Ordering::Relaxed);
 
         let mut blocking_dependents = index_mutex!(self.transactions_dependents, blocking_tx_idx);
         blocking_dependents.push(tx_idx);
@@ -278,26 +295,50 @@ impl<T: TaskProvider> ExeScheduler<T> {
     }
 
     fn add_blocking_task(&self, tx_idx: TxIdx) {
-        let mut tx = index_mutex!(self.transactions_status, tx_idx);
-        if tx.status != IncarnationStatus::ReadyToExecute {
-            tx.status = IncarnationStatus::ReadyToExecute;
-            tx.incarnation += 1;
+        let tx = self.transactions_status.get(tx_idx).unwrap();
+        let old_status = tx.load(Ordering::Relaxed);
+        let incarnation = old_status.incarnation + 1;
+        if old_status.status != IncarnationStatus::ReadyToExecute
+            && tx
+                .compare_exchange(
+                    old_status,
+                    TxStatus {
+                        incarnation,
+                        status: IncarnationStatus::ReadyToExecute,
+                    },
+                    Ordering::Relaxed,
+                    Ordering::Relaxed,
+                )
+                .is_ok()
+        {
             self.execution_queue.push(tx_idx);
         }
     }
 
     fn add_execution_task(&self, tx_idx: TxIdx) {
-        let mut tx = index_mutex!(self.transactions_status, tx_idx);
-        if tx.status == IncarnationStatus::Validated {
-            self.num_validated.fetch_sub(1, Ordering::Relaxed);
-        }
+        let tx = self.transactions_status.get(tx_idx).unwrap();
+        let old_status = tx.load(Ordering::Relaxed);
+        let is_validated = old_status.status == IncarnationStatus::Validated;
+        let incarnation = old_status.incarnation + 1;
         if matches!(
-            tx.status,
+            old_status.status,
             IncarnationStatus::Validated | IncarnationStatus::Executed
-        ) {
-            tx.status = IncarnationStatus::ReadyToExecute;
-            tx.incarnation += 1;
+        ) && tx
+            .compare_exchange(
+                old_status,
+                TxStatus {
+                    incarnation,
+                    status: IncarnationStatus::ReadyToExecute,
+                },
+                Ordering::Relaxed,
+                Ordering::Relaxed,
+            )
+            .is_ok()
+        {
             self.execution_queue.push(tx_idx);
+            if is_validated {
+                self.num_validated.fetch_sub(1, Ordering::Relaxed);
+            }
         }
     }
 
@@ -322,15 +363,18 @@ impl<T: TaskProvider> ExeScheduler<T> {
             self.add_execution_task(tx_idx);
         }
 
-        let mut tx = index_mutex!(self.transactions_status, tx_version.tx_idx);
-        debug_assert_eq!(tx.status, IncarnationStatus::Executing);
-        debug_assert_eq!(tx.incarnation, tx_version.tx_incarnation);
+        let tx = self.transactions_status.get(tx_version.tx_idx).unwrap();
+        let mut old_status = tx.load(Ordering::Relaxed);
+        debug_assert_eq!(old_status.status, IncarnationStatus::Executing);
+        debug_assert_eq!(old_status.incarnation, tx_version.tx_incarnation);
 
         if flags.contains(FinishExecFlags::NeedValidation) {
-            tx.status = IncarnationStatus::Executed;
+            old_status.status = IncarnationStatus::Executed;
+            tx.store(old_status, Ordering::Relaxed);
             return Some(Task::Validation(tx_version.tx_idx));
         } else {
-            tx.status = IncarnationStatus::Validated;
+            old_status.status = IncarnationStatus::Validated;
+            tx.store(old_status, Ordering::Relaxed);
             self.num_validated.fetch_add(1, Ordering::Relaxed);
         }
 
@@ -345,22 +389,48 @@ impl<T: TaskProvider> ExeScheduler<T> {
     // and the higher transactions for validation. The re-execution task is returned
     // for the aborted transaction.
     pub(crate) fn finish_validation(&self, tx_idx: TxIdx, aborted: bool) -> Option<Task> {
-        let mut tx = index_mutex!(self.transactions_status, tx_idx);
-        if aborted {
-            // TODO2: use function
-            if tx.status == IncarnationStatus::Executed {
-                tx.status = IncarnationStatus::Executing;
-                tx.incarnation += 1;
-                Some(Task::Execution(TxVersion {
-                    tx_idx,
-                    tx_incarnation: tx.incarnation,
-                }))
+        let tx = self.transactions_status.get(tx_idx).unwrap();
+        let old_status = tx.load(Ordering::Relaxed);
+        let incarnation = old_status.incarnation;
+
+        if old_status.status == IncarnationStatus::Executed {
+            if aborted {
+                if tx
+                    .compare_exchange(
+                        old_status,
+                        TxStatus {
+                            incarnation: incarnation + 1,
+                            status: IncarnationStatus::Executing,
+                        },
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    )
+                    .is_ok()
+                {
+                    return Some(Task::Execution(TxVersion {
+                        tx_idx,
+                        tx_incarnation: incarnation,
+                    }));
+                }
+                None
             } else {
+                if tx
+                    .compare_exchange(
+                        old_status,
+                        TxStatus {
+                            incarnation,
+                            status: IncarnationStatus::Validated,
+                        },
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    )
+                    .is_ok()
+                {
+                    self.num_validated.fetch_add(1, Ordering::Relaxed);
+                }
                 None
             }
         } else {
-            tx.status = IncarnationStatus::Validated;
-            self.num_validated.fetch_add(1, Ordering::Relaxed);
             None
         }
     }
---
+++ crates/pe/src/types.rs
@@ -0,0 +1,246 @@
+use alloy_primitives::{Address, B256, U256};
+use hashbrown::HashMap;
+use revm::state::AccountInfo;
+use smallvec::SmallVec;
+use std::fmt;
+use std::hash::{BuildHasherDefault, Hash, Hasher};
+use std::marker::PhantomData;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+pub struct AtomicWrapper<T: Into<usize> + From<usize>> {
+    inner: AtomicUsize,
+    _marker: PhantomData<T>,
+}
+
+impl<T: Into<usize> + From<usize>> AtomicWrapper<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            inner: AtomicUsize::new(value.into()),
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn load(&self, ordering: Ordering) -> T {
+        T::from(self.inner.load(ordering))
+    }
+
+    pub fn store(&self, value: T, ordering: Ordering) {
+        self.inner.store(value.into(), ordering);
+    }
+
+    pub fn swap(&self, value: T, ordering: Ordering) -> T {
+        T::from(self.inner.swap(value.into(), ordering))
+    }
+
+    pub fn compare_exchange(
+        &self,
+        current: T,
+        new: T,
+        success: Ordering,
+        failure: Ordering,
+    ) -> Result<T, T> {
+        self.inner
+            .compare_exchange(current.into(), new.into(), success, failure)
+            .map(T::from)
+            .map_err(T::from)
+    }
+}
+
+impl<T: Into<usize> + From<usize> + fmt::Debug> fmt::Debug for AtomicWrapper<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.load(Ordering::SeqCst).fmt(f)
+    }
+}
+
+/// We use the last 8 bytes of an existing hash like address
+/// or code hash instead of rehashing it.
+// TODO: Make sure this is acceptable for production
+#[derive(Debug, Default)]
+pub struct SuffixHasher(u64);
+impl Hasher for SuffixHasher {
+    fn write(&mut self, bytes: &[u8]) {
+        let mut suffix = [0u8; 8];
+        suffix.copy_from_slice(&bytes[bytes.len() - 8..]);
+        self.0 = u64::from_be_bytes(suffix);
+    }
+    fn finish(&self) -> u64 {
+        self.0
+    }
+}
+
+/// Build a suffix hasher
+pub type BuildSuffixHasher = BuildHasherDefault<SuffixHasher>;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum MemoryLocation {
+    // TODO: Separate an account's balance and nonce?
+    Basic(Address),
+    CodeHash(Address),
+    Storage(Address, U256),
+}
+
+// We only need the full memory location to read from storage.
+// We then identify the locations with its hash in the multi-version
+// data, write and read sets, which is much faster than rehashing
+// on every single lookup & validation.
+pub type MemoryLocationHash = u64;
+
+/// This is primarily used for memory location hash, but can also be used for
+/// transaction indexes, etc.
+#[derive(Debug, Default)]
+pub struct IdentityHasher(u64);
+impl Hasher for IdentityHasher {
+    fn write_u64(&mut self, id: u64) {
+        self.0 = id;
+    }
+    fn write_usize(&mut self, id: usize) {
+        self.0 = id as u64;
+    }
+    fn finish(&self) -> u64 {
+        self.0
+    }
+    fn write(&mut self, _: &[u8]) {
+        unreachable!()
+    }
+}
+
+/// Build an identity hasher
+pub type BuildIdentityHasher = BuildHasherDefault<IdentityHasher>;
+
+// TODO: It would be nice if we could tie the different cases of
+// memory locations & values at the type level, to prevent lots of
+// matches & potentially dangerous mismatch mistakes.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MemoryValue {
+    Basic(AccountInfo),
+    CodeHash(B256),
+    Storage(U256),
+    // We lazily update the beneficiary balance to avoid continuous
+    // dependencies as all transactions read and write to it. We also
+    // lazy update the senders & recipients of raw transfers, which are
+    // also common (popular CEX addresses, airdrops, etc).
+    // We fully evaluate these account states at the end of the block or
+    // when there is an explicit read.
+    // Explicit balance addition.
+    LazyRecipient(U256),
+    // Explicit balance subtraction & implicit nonce increment.
+    LazySender(U256),
+    // The account was self-destructed.
+    SelfDestructed,
+}
+
+#[derive(Debug)]
+pub enum MemoryEntry {
+    Data(TxIncarnation, MemoryValue),
+    // When an incarnation is aborted due to a validation failure, the
+    // entries in the multi-version data structure corresponding to its
+    // write set are replaced with this special ESTIMATE marker.
+    // This signifies that the next incarnation is estimated to write to
+    // the same memory locations. An incarnation stops and is immediately
+    // aborted whenever it reads a value marked as an ESTIMATE written by
+    // a lower transaction, instead of potentially wasting a full execution
+    // and aborting during validation.
+    // The ESTIMATE markers that are not overwritten are removed by the next
+    // incarnation.
+    Estimate,
+}
+
+// The index of the transaction in the block.
+// TODO: Consider downsizing to [u32].
+pub type TxIdx = usize;
+
+// The i-th time a transaction is re-executed, counting from 0.
+// TODO: Consider downsizing to [u32].
+pub type TxIncarnation = usize;
+
+// - ReadyToExecute(i) --try_incarnate--> Executing(i)
+// Non-blocked execution:
+//   - Executing(i) --finish_execution--> Executed(i)
+//   - Executed(i) --finish_validation--> Validated(i)
+//   - Executed/Validated(i) --try_validation_abort--> Aborting(i)
+//   - Aborted(i) --finish_validation(w.aborted=true)--> ReadyToExecute(i+1)
+// Blocked execution:
+//   - Executing(i) --add_dependency--> Aborting(i)
+//   - Aborting(i) --resume--> ReadyToExecute(i+1)
+#[derive(PartialEq, Debug)]
+pub enum IncarnationStatus {
+    ReadyToExecute = 0,
+    Executing,
+    Executed,
+    Validated,
+    Blocking,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct TxStatus {
+    pub incarnation: TxIncarnation,
+    pub status: IncarnationStatus,
+}
+
+impl From<usize> for TxStatus {
+    fn from(value: usize) -> Self {
+        let incarnation = value >> 8;
+        let status = match value & 0xFF {
+            0 => IncarnationStatus::ReadyToExecute,
+            1 => IncarnationStatus::Executing,
+            2 => IncarnationStatus::Executed,
+            3 => IncarnationStatus::Validated,
+            _ => IncarnationStatus::Blocking,
+        };
+        Self {
+            incarnation,
+            status,
+        }
+    }
+}
+
+impl From<TxStatus> for usize {
+    fn from(val: TxStatus) -> Self {
+        (val.incarnation << 8) | val.status as usize
+    }
+}
+
+// We maintain an in-memory multi-version data structure that stores for
+// each memory location the latest value written per transaction, along
+// with the associated transaction incarnation. When a transaction reads
+// a memory location, it obtains from the multi-version data structure the
+// value written to this location by the highest transaction that appears
+// before it in the block, along with the associated version. If no previous
+// transactions have written to a location, the value would be read from the
+// storage state before block execution.
+#[derive(Clone, Debug, PartialEq)]
+pub struct TxVersion {
+    pub tx_idx: TxIdx,
+    pub tx_incarnation: TxIncarnation,
+}
+
+// The origin of a memory read. It could be from the live multi-version
+// data structure or from storage (chain state before block execution).
+#[derive(Debug, PartialEq)]
+pub enum ReadOrigin {
+    MvMemory(TxVersion),
+    Storage,
+}
+
+// A scheduled worker task
+// TODO: Add more useful work when there are idle workers like near
+// the end of block execution, while waiting for a huge blocking
+// transaction to resolve, etc.
+#[derive(Debug)]
+pub enum Task {
+    Execution(TxVersion),
+    Validation(TxIdx),
+}
+
+// Most memory locations only have one read origin. Lazy updated ones like
+// the beneficiary balance, raw transfer senders & recipients, etc. have a
+// list of lazy updates all the way to the first strict/absolute value.
+pub type ReadOrigins = SmallVec<[ReadOrigin; 1]>;
+
+// For validation: a list of read origins (previous transaction versions)
+// for each read memory location.
+pub type ReadSet = HashMap<MemoryLocationHash, ReadOrigins, BuildIdentityHasher>;
+
+// The updates made by this transaction incarnation, which is applied
+// to the multi-version data structure at the end of execution.
+pub type WriteSet = Vec<(MemoryLocationHash, MemoryValue)>;
