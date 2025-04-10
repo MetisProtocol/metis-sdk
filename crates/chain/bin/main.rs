40edbccabc80cad01e9124d8f68ddfd150a90d57
dylenfu <dylenfu@126.com>
2025-04-10 20:59:38 +0800

    refactor bin/main.rs

--- crates/chain/bin/main.rs
+++ crates/chain/bin/main.rs
@@ -3,53 +3,36 @@
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
-use clap::Parser;
 use metis_chain::provider::ParallelExecutorBuilder;
-use reth::{args::RessArgs, cli::Cli, ress::install_ress_subprotocol};
-use reth_ethereum_cli::chainspec::EthereumChainSpecParser;
-use reth_node_builder::{Node, NodeHandle};
+use reth::cli::Cli;
 use reth_node_ethereum::EthereumNode;
 use reth_node_ethereum::node::EthereumAddOns;
 use tracing::info;
 
 fn main() {
     reth_cli_util::sigsegv_handler::install();
+
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
     if std::env::var_os("RUST_BACKTRACE").is_none() {
         unsafe { std::env::set_var("RUST_BACKTRACE", "1") };
     }
 
-    if let Err(err) =
-        Cli::<EthereumChainSpecParser, RessArgs>::parse().run(async move |builder, ress_args| {
-            info!(target: "reth::cli", "Launching node");
-            let node = EthereumNode::default();
-            let provider = ParallelExecutorBuilder::default();
-            let components = node.components_builder().executor(provider);
-            let launcher = builder
-                .with_types()
-                .with_components(components)
-                .with_add_ons(EthereumAddOns::default());
+    if let Err(err) = Cli::parse_args().run(async move |builder, _| {
+        info!(target: "reth::cli", "Launching node");
+        let handle = builder
+            // use the default ethereum node types
+            .with_types::<EthereumNode>()
+            // Configure the components of the node
+            // use default ethereum components but use our custom pool
+            .with_components(
+                EthereumNode::components().executor(ParallelExecutorBuilder::default()),
+            )
+            .with_add_ons(EthereumAddOns::default())
+            .launch()
+            .await?;
 
-            let NodeHandle {
-                node,
-                node_exit_future,
-            } = launcher.launch_with_debug_capabilities().await?;
-
-            // Install ress subprotocol.
-            if ress_args.enabled {
-                install_ress_subprotocol(
-                    ress_args,
-                    node.provider,
-                    node.block_executor,
-                    node.network,
-                    node.task_executor,
-                    node.add_ons_handle.engine_events.new_listener(),
-                )?;
-            }
-
-            node_exit_future.await
-        })
-    {
+        handle.wait_for_node_exit().await
+    }) {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
