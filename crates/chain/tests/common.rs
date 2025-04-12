4656e7a2fb5b16b0189d3f1b36f9e8f69f47aac7
dylenfu <dylenfu@126.com>
2025-04-12 22:38:14 +0800

    refactor common functions

--- crates/chain/tests/common.rs
+++ crates/chain/tests/common.rs
@@ -1,6 +1,49 @@
 use alloy_genesis::Genesis;
+use alloy_primitives::{B256, b256, hex};
+use futures_util::StreamExt;
+use reth::api::{EngineTypes, NodeTypes};
+use reth::builder::rpc::RethRpcAddOns;
+use reth::builder::{BlockBody, FullNode, FullNodeComponents};
+use reth::rpc::api::EngineEthApiClient;
 use reth_ethereum::chainspec::ChainSpec;
+use reth_ethereum::primitives::SignedTransaction;
+use reth_ethereum::provider::CanonStateSubscriptions;
+use std::error::Error;
 use std::sync::Arc;
+
+pub async fn send_compare_transaction<Node, AddOns, Engine>(
+    node: FullNode<Node, AddOns>,
+) -> Result<(), Box<dyn Error>>
+where
+    Engine: EngineTypes,
+    Node: FullNodeComponents<Types: NodeTypes<Payload = Engine>>,
+    AddOns: RethRpcAddOns<Node>,
+{
+    let mut notifications = node.provider.canonical_state_stream();
+
+    // submit tx through rpc
+    let raw_tx = hex!(
+        "02f876820a28808477359400847735940082520894ab0840c0e43688012c1adb0f5e3fc665188f83d28a029d394a5d630544000080c080a0a044076b7e67b5deecc63f61a8d7913fab86ca365b344b5759d1fe3563b4c39ea019eab979dd000da04dfc72bb0377c092d30fd9e1cab5ae487de49586cc8b0090"
+    );
+    let hash = EngineEthApiClient::<B256, B256>::send_raw_transaction(
+        &node.engine_http_client(),
+        raw_tx.into(),
+    )
+    .await?;
+    let expected = b256!("0xb1c6512f4fc202c04355fbda66755e0e344b152e633010e8fd75ecec09b63398");
+
+    println!("submitted transaction: {hash}");
+    println!("expected transaction: {expected}");
+
+    let head = notifications.next().await.unwrap();
+    //let tx = &head.tip().body().transactions().next().unwrap();
+    let tx = &head.tip().body().transactions().iter().next().unwrap();
+    println!("notification transaction {:?}", tx.tx_hash());
+
+    println!("mined transaction: {hash}");
+
+    Ok(())
+}
 
 pub fn custom_chain() -> Arc<ChainSpec> {
     let custom_genesis = r#"
--- crates/chain/tests/custom_evm_node.rs
+++ crates/chain/tests/custom_evm_node.rs
@@ -1,16 +1,10 @@
-use alloy_primitives::{B256, b256, hex};
-use futures_util::StreamExt;
 use reth::{
     builder::{NodeBuilder, NodeHandle},
-    rpc::api::EngineEthApiClient,
     tasks::TaskManager,
 };
-use reth_ethereum::{
-    node::{
-        EthereumNode,
-        core::{args::RpcServerArgs, node_config::NodeConfig},
-    },
-    provider::CanonStateSubscriptions,
+use reth_ethereum::node::{
+    EthereumNode,
+    core::{args::RpcServerArgs, node_config::NodeConfig},
 };
 use std::error::Error;
 
@@ -35,26 +29,5 @@ async fn test_custom_dev_node() -> Result<(), Box<dyn Error>> {
         .launch()
         .await?;
 
-    let mut notifications = node.provider.canonical_state_stream();
-
-    // submit tx through rpc
-    let raw_tx = hex!(
-        "02f876820a28808477359400847735940082520894ab0840c0e43688012c1adb0f5e3fc665188f83d28a029d394a5d630544000080c080a0a044076b7e67b5deecc63f61a8d7913fab86ca365b344b5759d1fe3563b4c39ea019eab979dd000da04dfc72bb0377c092d30fd9e1cab5ae487de49586cc8b0090"
-    );
-    let hash = EngineEthApiClient::<B256, B256>::send_raw_transaction(
-        &node.engine_http_client(),
-        raw_tx.into(),
-    )
-    .await?;
-    let expected = b256!("0xb1c6512f4fc202c04355fbda66755e0e344b152e633010e8fd75ecec09b63398");
-
-    println!("submitted transaction: {hash}");
-    println!("expected transaction: {expected}");
-
-    let head = notifications.next().await.unwrap();
-    let tx = &head.tip().body().transactions().next().unwrap();
-    println!("notification transaction {:?}", tx.as_ref());
-
-    println!("mined transaction: {hash}");
-    Ok(())
+    common::send_compare_transaction(node).await
 }
